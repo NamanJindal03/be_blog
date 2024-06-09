83f2b0d666dadc27802acb5ea8c44b593f00f191
Naman Jindal <namanjindal113@gmail.com>
2024-06-09 22:51:02 +0530

    completed verifyandfetchuser middleware

--- src/middlewares/verifyAndFetchUser.js
+++ src/middlewares/verifyAndFetchUser.js
@@ -1,4 +1,17 @@
-async function verifyAndFetchUser(){
-
+const jwt = require('jsonwebtoken')
+async function verifyAndFetchUser(req, res, next){
+    const token = req.headers.authorization.split(' ')[1];
+    if(!token){
+        return res.status(401).json({status: false, message: 'token is required'})
+    }
+    try{
+        const userData = await jwt.verify(token, process.env.JWT_SECRET);
+        console.log(userData);
+        req.user = userData;
+        next()
+    }
+    catch(err){
+        return res.status(401).json({status: false, message: 'user authorization failed'})
+    }
 }
 module.exports = {verifyAndFetchUser}

983d3cf0f3eb5e2675305cfdb3f18546f51a206f
Naman Jindal <namanjindal113@gmail.com>
2024-06-09 22:11:20 +0530

    user login completed

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,7 @@
 
 const User = require('../models/user.models');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const saltRounds = 10;
 
 async function signup(req, res){
@@ -8,7 +9,7 @@ async function signup(req, res){
     try{
         const isUserPresent = await User.findOne({email});
         if(isUserPresent){
-            res.status(400).json({
+            return res.status(400).json({
                 status: false, 
                 message: 'User already exists with the current Email',
                 error: 'User already exists with the current Email',
@@ -16,7 +17,7 @@ async function signup(req, res){
         }
         const cryptedPassword = await bcrypt.hash(password, saltRounds);
         const newUser = await User.create({username, password: cryptedPassword, email});
-        res.status(200).json({
+        return res.status(200).json({
             status: true, 
             message: 'User signedup succesfully',
             data: newUser
@@ -33,12 +34,51 @@ async function signup(req, res){
 }
 
 async function login(req, res){
-    res.send('login working')
     //extract the user details from the body
+    const {email, password, username} = req.body;
+    try{
+        const user = await User.findOne({
+            $or: [{username}, {email}]
+        })
+        if(!user){
+            return res.status(400).json({
+                status: false, 
+                message: 'Password email combination is not correct',
+                error: 'Password email combination is not correct',
+            })
+        }
+        const isPasswordCorrect = await bcrypt.compare(password, user.password)
+        if(!isPasswordCorrect){
+            return res.status(400).json({
+                status: false, 
+                message: 'Password email combination is not correct',
+                error: 'Password email combination is not correct',
+            })
+        }
+        const authorizationToken = await jwt.sign({_id: user._id, email: user.email, username: user.username}, process.env.JWT_SECRET);
+        return res.status(200).json({
+            status: true, 
+            message: 'User login successfull',
+            token: authorizationToken,
+            data: user
+        })
+    }
+    catch(err){
+        console.error(err.message);
+        res.status(500).json({
+            status: false, 
+            message: 'User login failed, try again later',
+            error: err.message,
+        })
+    }
+
     //find the user from the email
     //compare the password through bcrypt
     //generate the jwt token | sign a token
     //handle all the error handling
+
+
+    //process.env.JWT_SECRET
 }
 
 async function getUserDetails(req, res){
